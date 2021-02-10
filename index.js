ecf6edddfcdb99d39726a0f5d8f8bc20e858a5a8
edougher <ejdougher@gmail.com>
2021-02-10 12:28:44 -0600

    Done.

--- index.js
+++ index.js
@@ -14,3 +14,14 @@ const tutorials = [
 const titleCased = () => {
   return tutorials
 }
+
+// *solution*
+//const titleCased = (input) => {
+//  return tutorials.map( line => {
+//    let tokens = line.split(' ')
+//    let capitalizedTokens =
+//      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+//    let response = capitalizedTokens.join(' ')
+//    return response
+//  })
+//}
